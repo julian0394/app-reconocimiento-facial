a97c4e3e312e3057f31e7476f459ae19301fa95c
julian0394 <julian0394@hotmail.com>
2021-04-01 14:51:03 -0300

    agregado el state en App e InputImagen - previo a api de reconocimiento

--- app-reconocimiento-facial/src/App.jsx
+++ app-reconocimiento-facial/src/App.jsx
@@ -1,25 +1,32 @@
-// import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-
 import Cabecera from './Componentes/cabecera/Cabecera';
-// import Logo from './Componentes/cabecera/logo/Logo';
 import Usuario from './Componentes/usuario/Usuario';
 import InputImagen from './Componentes/inputImagen/InputImagen';
 import Reconocimiento from './Componentes/reconocimiento/Reconocimiento';
 import FondoParticulas from './Componentes/fondoParticulas/FondoParticulas';
 
 function App() {
-  return (
-    <div className="App">
-        <Cabecera  />
-        <Usuario />
-        <InputImagen />
-        {/* <Reconocimiento /> */}
-        <div className="div-fondo-particulas">
-            <FondoParticulas />
+    //state del link pasado por el usuario desde #InputImagen
+    const [link, setLink] = useState('');
+
+    function manejoCambioInput(nuevoLink) {
+        setLink(nuevoLink);
+        // console.log('clickeado', nuevoLink);
+    }
+
+    return (
+        <div className="App">
+            <h1>{link}</h1>
+            <Cabecera  />
+            <Usuario />
+            <InputImagen alEscanear={manejoCambioInput} />
+            {/* <Reconocimiento data={link} /> */}
+            <div className="div-fondo-particulas">
+                <FondoParticulas />
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- app-reconocimiento-facial/src/Componentes/inputImagen/InputImagen.jsx
+++ app-reconocimiento-facial/src/Componentes/inputImagen/InputImagen.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './inputImagen.css'
 
-function InputImagen() {
+function InputImagen(props) {
+    //state del input que el usuario carga con un link
+    const [inputUsuario, setInputUsuario] = useState('');
+
+    function manejoClickEscanear(event) {
+        props.alEscanear(inputUsuario);
+        setInputUsuario('');
+        event.preventDefault();
+    }
+
     return (  
         <div className="div-externo-input">
             <form>
-                <input type="text" placeholder="Ingrese el link de una foto" /> {/* size="40" */}
-                <button>Escanear</button>
+                <input 
+                    onChange={ (event) => setInputUsuario(event.target.value) }
+                    type="text" 
+                    placeholder="Ingrese el link de una foto"
+                    value={inputUsuario} 
+                />
+                <button onClick={manejoClickEscanear}>Escanear</button> 
             </form>
         </div>
     ); 
